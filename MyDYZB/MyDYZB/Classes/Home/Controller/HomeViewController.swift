1623c27a0fdd8e36146096828ba66024063116d6
WangLei <maybe_mylove@163.com>
2017-03-11 20:39:03 +0800

    MVC形式进行项目分层

---
+++ MyDYZB/MyDYZB/Classes/Home/Controller/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  MyDYZB
+//
+//  Created by maybe_mylove on 2017/3/11.
+//  Copyright © 2017年 maybe_mylove. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
+// MARK:  设置UI界面
+extension HomeViewController{
+    private func setupUI(){
+    
+    }
+    private func setupNavigationBar(){
+    
+    }
+}
